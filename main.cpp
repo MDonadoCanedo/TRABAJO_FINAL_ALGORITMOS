261e5d28e3f83234a8cae612c5a3cd7d5bd78158
Mauricio Donado Canedo <84418495+MDonadoCanedo@users.noreply.github.com>
2023-05-10 21:41:59 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
-#include "octal.h"
 #include "binario.h"
 #include "decimal.h"
 #include "hexadecimal.h"
+#include "octal.h"
 
 int main()
 {
@@ -21,70 +21,72 @@ int main()
  
  while(opc != 5)
  {
-   cout << endl;
-  cout << "---------------SISTEMAS NUMERICOS----------------" << endl;
-  cout << "-------------------------------------------------" << endl << endl;
-    cout << "\nSeleccione el sistema numerico  que desea utilizar \n"<<endl;
-  cout << "Clase binario.................................(1)" << endl;
-  cout << "Clase decimal...................................(2)" << endl;
-  cout << "Clase octal.................................(3)" << endl;
-  cout << "Clase hexadecimal.............................(4)" << endl;
-  cout << "Salir / Exit..................................(5)" << endl << endl;
-   cout << endl << "Elija la opcion que desea realizar: ";
+    cout << "-------------------------------------------------" << endl;
+    cout << "|                SISTEMAS NUMÉRICOS               |" << endl;
+    cout << "-------------------------------------------------" << endl;
+    cout << "| Seleccione el sistema numérico que desea usar: |" << endl;
+    cout << "-------------------------------------------------" << endl;
+    cout << "|                1. Binario                      |" << endl;
+    cout << "|                2. Decimal                      |" << endl;
+    cout << "|                3. Octal                        |" << endl;
+    cout << "|                4. Hexadecimal                  |" << endl;
+    cout << "|                5. Salir                        |" << endl;
+    cout << "-------------------------------------------------" << endl;
+    cout << "Opción: ";
    cin >> opc;
    switch(opc)
    {
      case 1: 
-      cout <<"\n\n***Valores a usar***";
-      cout << "\nn1 es " << Bn1;
-      cout << "\nn2 es " << Bn2;
-      cout << "\nn3 es " << Bn3;
+      cout <<"\nValores a usar";
+      cout << "\nn1 es:  " << Bn1;
+      cout << "\nn2 es:  " << Bn2;
+      cout << "\nn3 es:  " << Bn3;
       Bn3 = Bn1 + Bn2;
       cout<<endl; 
-      cout<<"\n***Resultado***";
+      cout<<"\nResultado: ";
       cout << "\n" << Bn1 << " + " << Bn2 << " = " << Bn3;
        break;
      
      case 2:
-      cout <<"\n\n***Valores a usar***";
-      cout << "\nn1 es " << dn1;
-      cout << "\nn2 es " << dn2;
-      cout << "\nn3 es " << dn3;
+      cout <<"\nValores a usar: ";
+      cout << "\nn1 es:     " << dn1;
+      cout << "\nn2 es:     " << dn2;
+      cout << "\nn3 es:     " << dn3;
       dn3 = dn1 + dn2;
        cout<<endl; 
-      cout<<"\n***Resultado***";
+      cout<<"\nResultado";
       cout << "\n" << dn1 << " + " << dn2 << " = " << dn3;
       cout<<"\n\n";
        break;
      
      case 3:   
-      cout <<"\n\n***Valores a usar***";
+      cout <<"\n\nValores a usar: ";
       cout << "\nn1 es " << On1;
       cout << "\nn2 es " << On2;
       cout << "\nn3 es " << On3;
       On3 = On1 + On2;
       cout<<endl;  
-       cout<<"\n***Resultado***";
+       cout<<"\nResultado";
       cout << "\n" << On1 << " + " << On2 << " = " << On3;
       cout<<"\n\n";
       break;
      
      case 4:
-      cout <<"\n\n***Valores a usar***";
-      cout << "\nn1 es " << hn1;
-      cout << "\nn2 es " << hn2;
-      cout << "\nn3 es " << hn3;
+      cout <<"\n\nValores a usar";
+      cout << "\nn1 es:     " << hn1;
+      cout << "\nn2 es:     " << hn2;
+      cout << "\nn3 es:     " << hn3;
       hn3 = hn1 + hn2;
       cout<<endl;  
-      cout<<"\n***Resultado***"; 
+      cout<<"\nResultado"; 
       cout << "\n" << hn1 << " + " << hn2 << " = " << hn3;
       break;
 
       case 5:
-        cout << "\n\nGracias por usar nuestros servicios" << endl;
+        cout << "\nServicio Finalizado :)   " << endl;
         break;
      default:
-       cout << "\n\n\t¡ERROR EN OPCION!";
+       cout << "\n\n\tERROR EN OPCION";
        opc = '0';
      
    }
